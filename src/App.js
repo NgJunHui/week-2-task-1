030aa27902e950225388de54ceeeaa998ad7fffa
NgJunHui <ngjunhui96@gmail.com>
2022-10-17 17:37:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  employeeData = [
+    { name: "Jun Hui", age: 26, designation: "Developer", department: "IT" },
+    { name: "John", age: 32, designation: "Manager", department: "Finance" },
+    { name: "Dominic", age: 35, designation: "Engineer", department: "IT" }
+  ];
+
+  state = {
+    name: "Jack",
+    coffee: "Espresso",
+    store: "Starbucks"
+  }
+  handleClick = () => {
+    this.setState({
+      coffee: "Latte",
+      store: 'Macdonalds'
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.employeeData.map(({ name, age, designation, department }) => (
+          <ul>
+            <li>
+              <p key={name}>My name is {name}, {age} years old.
+                I am a {designation} in the {department} department</p>
+            </li>
+          </ul>
+        ))}
+        <p> My name is {this.state.name}, I love to drink {this.state.coffee}, from {this.state.store}</p>
+        <button onClick={this.handleClick}>Update</button>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
